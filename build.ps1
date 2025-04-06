fc612869fcb0bd2ee577d7a3e798e104843e1da3
Yann M <montiel.yann@gmail.com>
2025-04-06 12:49:22 +0200

    update to powershell scripts (and stuart_ci cmds)

---
+++ build.ps1
@@ -0,0 +1,21 @@
+# Setup
+Set-Location edk2
+
+# Activate virtual environment
+. .venv\Scripts\Activate.ps1
+
+# Install Python dependencies
+pip install -r pip-requirements.txt --upgrade
+
+# Setup and update workspace
+stuart_setup -c .pytool/CISettings.py
+stuart_update -c .pytool/CISettings.py -a X64 TOOL_CHAIN_TAG=VS2022
+
+# Build
+stuart_ci_build -c .pytool/CISettings.py -p MdeModulePkg -t RELEASE -a X64 TOOL_CHAIN_TAG=VS2022
+
+# Copy resulting EFI binary
+Copy-Item -Force "Build\MdeModule\RELEASE_VS2019\X64\PowerMonkey.efi" "..\EFI\Boot"
+
+# Return to root directory
+Set-Location ..
---
+++ setup.ps1
@@ -0,0 +1,27 @@
+# Download Edk2
+git clone https://github.com/tianocore/edk2.git
+cd edk2
+git submodule update --init
+cd ..
+
+# Download PowerMonkey
+git clone https://github.com/psyq321/PowerMonkey.git
+Copy-Item PowerMonkey\PowerMonkeyApp\CONFIGURATION.c .
+Move-Item PowerMonkey\PowerMonkeyApp edk2\MdeModulePkg\Application
+(Get-Content edk2/MdeModulePkg/MdeModulePkg.dsc) -replace '/HelloWorld.inf', "/HelloWorld.inf`r`n  MdeModulePkg/Application/PowerMonkeyApp/PowerMonkey.inf`r`n" | Set-Content edk2/MdeModulePkg/MdeModulePkg.dsc
+"ACTIVE_PLATFORM       = MdeModulePkg/MdeModulePkg.dsc" | Set-Content edk2/Conf/target.txt
+
+# Download shell.efi
+New-Item -ItemType Directory -Force -Path EFI\Boot
+Invoke-WebRequest -Uri https://github.com/tianocore/edk2/raw/UDK2018/ShellBinPkg/UefiShell/X64/Shell.efi -OutFile EFI\Boot\shellx64.efi
+Copy-Item startup.nsh EFI\Boot\
+
+# Prepare Python
+cd edk2
+python -m venv .venv
+. .venv\Scripts\Activate.ps1
+pip install -r pip-requirements.txt --upgrade
+
+# Build Base Tools
+stuart_setup -c .pytool/CISettings.py
+python BaseTools/Edk2ToolsBuild.py -a X64
